9c9d119c53bbb9d773bcff0f9f909749b7ffa21f
hossmi <hossmi@gmail.com>
2017-04-02 00:31:48 +0200

    ILogger interface, NullLogger and default LogLevel

---
+++ QTFK.Core/Services/Loggers/NullLogger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QTFK.Services.Loggers
+{
+    public class NullLogger<T> : ILogger<T>
+    {
+        private static NullLogger<T> _instance;
+
+        private NullLogger() { }
+
+        public static ILogger<T> Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new NullLogger<T>();
+                return _instance;
+            }
+        }
+
+        public void Log(T level, string message) { }
+    }
+}
