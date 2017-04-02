a4e78d5e4d12e17e510d998ba08f48d1f9268916
hossmi <hossmi@gmail.com>
2017-04-02 19:28:59 +0200

    Inittial IDBIO interface

---
+++ QTFK.Core/Services/IDBIO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QTFK.Services
+{
+    public interface IDBIO
+    {
+        int Set(Func<IDbCommand, int> instruction);
+        T Get<T>(string query, IDataReader reader);
+        DataSet Get(string query);
+    }
+}
