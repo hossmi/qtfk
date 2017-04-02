17970c8af6e1295ba7b2fb04f0429e957d9b3a48
hossmi <hossmi@gmail.com>
2017-04-02 20:53:06 +0200

    Inittial SQLServer IDBIO implementation, project and test proyect

---
+++ QTFK.Services.DBIO.SQLServer/Services/DBIO/SQLServerDBIO.cs
@@ -0,0 +1,105 @@
+﻿using QTFK.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using QTFK.Models;
+
+namespace QTFK.Services.DBIO
+{
+    public class SQLServerDBIO : IDBIO
+    {
+        private readonly string _connectionString;
+
+        public SQLServerDBIO(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public DataSet Get(string query)
+        {
+            DataSet ds = null;
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            using (SqlDataAdapter da = new SqlDataAdapter(query, conn))
+            {
+                try
+                {
+                    ds = new DataSet();
+                    da.Fill(ds);
+                }
+                catch (Exception ex)
+                {
+                    throw new DBIOException($@"Error ocurred executing SQL instructions:
+Exception: {ex.GetType().Name}
+Query: '{query ?? ""}'", ex);
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+            return ds;
+        }
+
+        public IEnumerable<T> Get<T>(string query, Func<IDataReader, T> build)
+        {
+            SqlConnection conn = new SqlConnection(_connectionString);
+            SqlCommand command = new SqlCommand() { Connection = conn };
+            conn.Open();
+            command.CommandText = query;
+            var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                T item = default(T);
+                try
+                {
+                    item = build(reader);
+                }
+                catch (Exception ex)
+                {
+                    throw new DBIOException($@"Error ocurred building object from {nameof(IDataReader)}:
+Exception: {ex.GetType().Name}
+T: {typeof(T).Name}
+Current command: {command?.CommandText ?? ""}", ex);
+                }
+                yield return item;
+            }
+            conn.Close();
+        }
+
+        public int Set(Func<IDbCommand, int> instruction)
+        {
+            int affectedRows = 0;
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            using (SqlCommand command = new SqlCommand() { Connection = conn })
+            {
+                SqlTransaction trans = null;
+                try
+                {
+                    conn.Open();
+                    trans = conn.BeginTransaction();
+                    command.Transaction = trans;
+                    affectedRows = instruction(command);
+                    trans.Commit();
+                }
+                catch (Exception ex)
+                {
+                    trans?.Rollback();
+
+                    throw new DBIOException($@"Error ocurred executing SQL instructions:
+Exception: {ex.GetType().Name}
+Current command: {command?.CommandText ?? ""}", ex);
+                }
+                finally
+                {
+                    conn.Close();
+                }
+
+            }
+            return affectedRows;
+        }
+    }
+}
---
+++ Tests/QTFK.Services.DBIO.SQLServer.Tests/SQLServerTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QTFK.Services;
+using QTFK.Services.DBIO;
+
+namespace QTFK.Services.DBIO.SQLServer.Tests
+{
+    [TestClass]
+    public class SQLServerTests
+    {
+        private readonly string _connectionString;
+
+        public SQLServerTests()
+        {
+            _connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["tests"].ConnectionString;
+            if (string.IsNullOrWhiteSpace(_connectionString))
+                throw new ArgumentException($"Empty or invalid 'tests' connection string in app.config", "tests");
+
+
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IDBIO db = new SQLServerDBIO(_connectionString);
+            
+        }
+    }
+}
