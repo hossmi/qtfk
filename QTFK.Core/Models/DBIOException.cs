1d44ea057c45b1c05297689cbed2fb261152257e
hossmi <hossmi@gmail.com>
2017-04-02 20:51:17 +0200

    Added DBIOException

---
+++ QTFK.Core/Models/DBIOException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace QTFK.Models
+{
+    [Serializable]
+    public class DBIOException : Exception
+    {
+        public DBIOException()
+        {
+        }
+
+        public DBIOException(string message) : base(message)
+        {
+        }
+
+        public DBIOException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected DBIOException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
