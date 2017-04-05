5a77c0445ceae9629325d556048a60164539e4fa
hossmi <hossmi@gmail.com>
2017-04-05 18:59:58 +0200

    First SQL Server test class with cleanup and initialize methods

--- Tests/QTFK.Services.DBIO.SQLServer.Tests/SQLServerTests.cs
+++ Tests/QTFK.Services.DBIO.SQLServer.Tests/SQLServerTests.cs
@@ -2,6 +2,11 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using QTFK.Services;
 using QTFK.Services.DBIO;
+using QTFK.Extensions.DBIO;
+using System.Collections.Generic;
+using System.Text;
+using QTFK.Extensions.Collections.Filters;
+using QTFK.Extensions.DBCommand;
 
 namespace QTFK.Services.DBIO.SQLServer.Tests
 {
@@ -9,20 +14,50 @@ namespace QTFK.Services.DBIO.SQLServer.Tests
     public class SQLServerTests
     {
         private readonly string _connectionString;
+        private readonly IDBIO _db;
 
         public SQLServerTests()
         {
-            _connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["tests"].ConnectionString;
+            _connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["tests"]?.ConnectionString;
             if (string.IsNullOrWhiteSpace(_connectionString))
                 throw new ArgumentException($"Empty or invalid 'tests' connection string in app.config", "tests");
 
+            _db = new QTFK.Services.DBIO.SQLServerDBIO(_connectionString);
+        }
 
+        [TestInitialize()]
+        public void Create()
+        {
+            Drop();
+            _db.SetInBlock(File.ReadBlocks("create.sql", "go"));
+        }
+
+
+        [TestCleanup()]
+        public void Drop()
+        {
+            _db.SetIndividually(File.ReadBlocks("drop.sql", "go"), false);
         }
 
         [TestMethod]
         public void TestMethod1()
         {
-            IDBIO db = new SQLServerDBIO(_connectionString);
+            _db.Set(cmd =>
+            {
+                cmd.CommandText = $@"
+                    USE qtfk
+                    INSERT INTO persona (nombre, apellidos)
+                    VALUES (@nombre,@apellidos)
+                    ;";
+                cmd.AddParameters(new Dictionary<string, object>
+                {
+                    { "@nombre", "pepe" },
+                    { "@apellidos", "De la rosa Castaños" },
+                });
+
+                cmd.ExecuteNonQuery();
+            });
+
             
         }
     }
---
+++ Tests/QTFK.Services.DBIO.SQLServer.Tests/create.sql
@@ -0,0 +1,52 @@
+﻿use qtfk
+go
+
+CREATE TABLE dbo.persona(
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[nombre] [nvarchar](256) NULL,
+	[apellidos] [nvarchar](512) NULL,
+	[fecha_nacimiento] [date] NULL,
+	[hora_nacimiento] [time](7) NULL,
+ CONSTRAINT [PK_persona] PRIMARY KEY CLUSTERED ([id])
+) 
+
+GO
+
+CREATE TABLE dbo.[etiqueta](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[nombre] [nvarchar](256) NULL,
+ CONSTRAINT [PK_etiqueta] PRIMARY KEY CLUSTERED ([id] )
+)
+
+GO
+
+CREATE TABLE dbo.[etiquetas_personas](
+	[persona_id] [int] NOT NULL,
+	[etiqueta_id] [int] NOT NULL,
+ CONSTRAINT [PK_etiquetas_personas] PRIMARY KEY CLUSTERED ([persona_id] ASC,[etiqueta_id] ASC)
+) 
+
+GO
+
+ALTER TABLE dbo.[etiquetas_personas]  WITH CHECK 
+ADD  CONSTRAINT [FK_etiquetas_personas__etiqueta] 
+FOREIGN KEY([etiqueta_id])
+REFERENCES dbo.[etiqueta] ([id])
+ON UPDATE CASCADE
+ON DELETE CASCADE
+GO
+
+ALTER TABLE dbo.[etiquetas_personas] CHECK CONSTRAINT [FK_etiquetas_personas__etiqueta]
+GO
+
+ALTER TABLE dbo.[etiquetas_personas]  WITH CHECK 
+ADD  CONSTRAINT [FK_etiquetas_personas__persona] 
+FOREIGN KEY([persona_id])
+REFERENCES dbo.[persona] ([id])
+ON UPDATE CASCADE
+ON DELETE CASCADE
+GO
+
+ALTER TABLE dbo.[etiquetas_personas] CHECK CONSTRAINT [FK_etiquetas_personas__persona]
+GO
+
