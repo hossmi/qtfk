b19aa8248b5d55b8587f6828439fd0f2b4226c76
hossmi <hossmi@gmail.com>
2017-04-05 23:57:00 +0200

    Upgraded File.ReadBlocks. It is no its responsability to trim and filter lines. Also checks if delimiter is empty, returning whole content if true.

--- QTFK.Extensions/File.cs
+++ QTFK.Extensions/File.cs
@@ -11,15 +11,19 @@ namespace QTFK
     {
         public static IEnumerable<string> ReadBlocks(string fileName, string delimiter)
         {
-            delimiter = delimiter.ToLower();
+            delimiter = delimiter.Trim().ToLower();
+            if (string.IsNullOrWhiteSpace(delimiter))
+            {
+                yield return System.IO.File.ReadAllText(fileName);
+                yield break;
+            }
+
             var sb = new StringBuilder();
             foreach (string line in System.IO.File
                 .ReadLines(fileName, Encoding.UTF8)
-                .NotEmpty()
-                .Trim()
                 )
             {
-                if (line.ToLower() == delimiter)
+                if (line.Trim().ToLower() == delimiter)
                 {
                     yield return sb.ToString();
                     sb.Clear();
