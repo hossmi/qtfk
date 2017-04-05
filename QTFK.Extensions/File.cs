d3880e85b40f687a82e257f9dabba6fd95e1f75f
hossmi <hossmi@gmail.com>
2017-04-05 18:57:09 +0200

    Static File class for more IO

---
+++ QTFK.Extensions/File.cs
@@ -0,0 +1,34 @@
+﻿using QTFK.Extensions.Collections.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QTFK
+{
+    public static class File
+    {
+        public static IEnumerable<string> ReadBlocks(string fileName, string delimiter)
+        {
+            delimiter = delimiter.ToLower();
+            var sb = new StringBuilder();
+            foreach (string line in System.IO.File
+                .ReadLines(fileName, Encoding.UTF8)
+                .NotEmpty()
+                .Trim()
+                )
+            {
+                if (line.ToLower() == delimiter)
+                {
+                    yield return sb.ToString();
+                    sb.Clear();
+                }
+                else
+                {
+                    sb.AppendLine(line);
+                }
+            }
+        }
+    }
+}
