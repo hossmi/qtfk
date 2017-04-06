f7870e7845892169c9b4c72d010c47b05e9f6b51
hossmi <hossmi@gmail.com>
2017-04-06 15:56:28 +0200

    Renamed DebugWindowsLogger to DebugLogger

---
+++ QTFK.Core/Services/Loggers/DebugLogger.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using QTFK.Models;
+
+namespace QTFK.Services.Loggers
+{
+    public class DebugLogger<T> : ILogger<T>
+    {
+        public DebugLogger(string category = null)
+        {
+            Category = category;
+        }
+
+        public string Category { get; set; }
+
+        public void Log(T level, string message)
+        {
+            Debug.WriteLine($"<<{level}>>: {message}", Category);
+        }
+    }
+
+    public class DebugLogger : DebugLogger<LogLevel>
+    {
+        public DebugLogger(string category = null) : base(category) { }
+    }
+}
