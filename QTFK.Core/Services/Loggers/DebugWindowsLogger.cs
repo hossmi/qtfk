66671276b00bcb004bdce96893e0694c389f0e7c
hossmi <hossmi@gmail.com>
2017-04-06 14:49:27 +0200

    Added DebugWindowsLogger

---
+++ QTFK.Core/Services/Loggers/DebugWindowsLogger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using QTFK.Services;
+using QTFK.Models;
+
+namespace QTFK.Services.Loggers
+{
+    public class DebugWindowsLogger<T> : ILogger<T>
+    {
+        public DebugWindowsLogger(string category = null)
+        {
+            Category = category;
+        }
+
+        public string Category { get; set; }
+
+        public void Log(T level, string message)
+        {
+            Debug.WriteLine($"<<{level}>>: {message}", Category);
+        }
+    }
+
+    public class DebugWindowsLogger : DebugWindowsLogger<LogLevel>
+    {
+        public DebugWindowsLogger(string category = null) : base(category) { }
+    }
+}
