55ef25aa19212d6c9913a10930436146c2ef8860
hossmi <hossmi@gmail.com>
2017-04-08 18:09:35 +0200

    OleDB Tests

---
+++ Tests/QTFK.Services.DBIO.OleDB.Tests/Models/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace QTFK.Services.DBIO.OleDB.Tests.Models
+{
+    public class Person
+    {
+        public string LastName { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Tests/QTFK.Services.DBIO.OleDB.Tests/drop.sql
@@ -0,0 +1,11 @@
+﻿
+DROP TABLE [etiquetas_personas]
+GO
+
+DROP TABLE [persona]
+GO
+
+DROP TABLE [etiqueta]
+GO
+
+
