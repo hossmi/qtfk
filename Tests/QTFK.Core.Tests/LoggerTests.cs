684f1f835f30cb01c30d48eb219f09b3778266ea
hossmi <hossmi@gmail.com>
2017-04-13 17:59:42 +0200

    Logger<LogLevel> Extension

--- Tests/QTFK.Core.Tests/LoggerTests.cs
+++ Tests/QTFK.Core.Tests/LoggerTests.cs
@@ -4,6 +4,7 @@ using QTFK.Services;
 using QTFK.Models;
 using QTFK.Services.Loggers;
 using System.Collections.Generic;
+using QTFK.Extensions.Loggers;
 
 namespace QTFK.Core.Tests
 {
@@ -58,6 +59,36 @@ Booooom!!
             log.Log(LogLevel.Warning, "loggin a warning");
             log.Log(LogLevel.Error, "Ups! an Error!");
             log.Log(LogLevel.Fatal, "Booooom!!");
+
+            buffer.Flush();
+            string output = buffer.ToString();
+            string expected = $@"loggin debug message
+loggin information message
+loggin a warning
+Ups! an Error!
+Booooom!!
+";
+            Assert.AreEqual(expected, output);
+        }
+
+        [TestMethod]
+        [TestCategory("Loggers")]
+        public void ConsoleLogger_extension_Tests()
+        {
+            var buffer = new System.IO.StringWriter();
+            Console.SetOut(buffer);
+
+            ILogger<LogLevel> log = new ConsoleLogger(new Dictionary<LogLevel, ConsoleColor>
+            {
+                { LogLevel.Error, ConsoleColor.Red },
+                { LogLevel.Fatal, ConsoleColor.Red },
+            });
+
+            log.Debug("loggin debug message");
+            log.Info("loggin information message");
+            log.Warning("loggin a warning");
+            log.Error("Ups! an Error!");
+            log.Fatal("Booooom!!");
 
             buffer.Flush();
             string output = buffer.ToString();
