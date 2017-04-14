d14d5a312323b05e33b4cd18f59858b38d2cad5a
Guillermo Mestre <warcraker@gmail.com>
2017-04-14 17:54:06 +0200

    Removed failsafe logger
    Let's follow the fail-faster approach.

--- QTFK.Core/Services/Loggers/FileLogger.cs
+++ QTFK.Core/Services/Loggers/FileLogger.cs
@@ -11,24 +11,19 @@ namespace QTFK.Services.Loggers
     {
         private readonly String _filepath;
         private readonly Func<T, String, String> _messageBuilder;
-        private readonly ILogger<T> _failSafeLogger;
 
         /// <summary>
         /// Initializes a new instance of <see cref="FileLogger{T}"/> 
         /// </summary>
         /// <param name="filepath">Path to the file were the logs will be stored</param>
         /// <param name="messageBuilder">Defines how the message will be crafted. By default: [time] level | message</param>
-        /// <param name="failSafeLogger"><see cref="ILogger{T}"/> to use if this one fails. 
-        /// It will log one entry for the exception thrown, and another one with the original entry. 
-        /// It is encouraged to use one that won't throw. No logger will be used by default</param>
-        public FileLogger(String filepath, Func<T, String, String> messageBuilder = null, ILogger<T> failSafeLogger = null)
+        public FileLogger(String filepath, Func<T, String, String> messageBuilder = null)
         {
             if (String.IsNullOrEmpty(filepath))
                 throw new ArgumentNullException(nameof(filepath));
 
             _filepath = filepath;
             _messageBuilder = messageBuilder ?? _defaultMessageBuilder;
-            _failSafeLogger = failSafeLogger ?? NullLogger<T>.Instance;
         }
 
         /// <summary>
@@ -38,15 +33,7 @@ namespace QTFK.Services.Loggers
         /// <param name="message"></param>
         public void Log(T level, string message)
         {
-            try
-            {
-                System.IO.File.AppendAllLines(_filepath, new[] { _messageBuilder(level, message) });
-            }
-            catch(Exception ex)
-            {
-                _failSafeLogger.Log(default(T), $"{nameof(FileLogger)} threw an exception trying to log: {ex.Message}");
-                _failSafeLogger.Log(level, message);
-            }
+            System.IO.File.AppendAllLines(_filepath, new[] { _messageBuilder(level, message) });
         }
 
         private static Func<T, String, String> _defaultMessageBuilder
@@ -61,8 +48,8 @@ namespace QTFK.Services.Loggers
     /// </summary>
     public class FileLogger : FileLogger<LogLevel>
     {
-        public FileLogger(String filepath, Func<LogLevel, String, String> messageBuilder = null, ILogger<LogLevel> failSafeLogger = null)
-            : base(filepath, messageBuilder, failSafeLogger)
+        public FileLogger(String filepath, Func<LogLevel, String, String> messageBuilder = null)
+            : base(filepath, messageBuilder)
         {
 
         }
