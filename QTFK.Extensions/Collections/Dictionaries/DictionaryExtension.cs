876a2c21db0397a0a87c1ca74dd3c562f2f590e1
Pablo Jimena López <pablo.jimena.lopez@everis.com>
2017-04-19 12:54:59 +0200

    Added overload methods for DictionaryExtension

--- QTFK.Extensions/Collections/Dictionaries/DictionaryExtension.cs
+++ QTFK.Extensions/Collections/Dictionaries/DictionaryExtension.cs
@@ -8,26 +8,50 @@ namespace QTFK.Extensions.Collections.Dictionaries
 {
     public static class DictionaryExtension
     {
-        public static void Get<T>(this IDictionary<string, object> dic, string key, Action<T> setter)
+        public static T Get<T>(this IDictionary<string, object> dic, string key)
+        {
+            return Get<T>(dic, key, default(T));
+        }
+        public static T Get<T>(this IDictionary<string, object> dic, string key, T defaultValue)
+        {
+            object value = null;
+            if (dic.TryGetValue(key, out value))
+                return (T)value;
+            else
+                return defaultValue;
+        }
+
+        public static IDictionary<string, object> Get<T>(this IDictionary<string, object> dic, string key, Action<T> setter)
         {
             Get<T>(dic, key, default(T), setter);
+            return dic;
         }
-        public static void Get<T>(this IDictionary<string, object> dic, string key, T defaultValue, Action<T> setter)
+        public static IDictionary<string, object> Get<T>(this IDictionary<string, object> dic, string key, T defaultValue, Action<T> setter)
         {
             object value = null;
             if(dic.TryGetValue(key, out value))
                 setter((T)value);
             else
                 setter(defaultValue);
+            return dic;
         }
 
-        public static void Get(this IDictionary<string, string> dic, string key, Action<string> setter)
+        public static IDictionary<string, string> Get(this IDictionary<string, string> dic, string key, Action<string> setter)
         {
             string value = string.Empty;
             if (dic.TryGetValue(key, out value))
                 setter(value);
             else
                 setter(string.Empty);
+            return dic;
+        }
+        public static string Get(this IDictionary<string, string> dic, string key)
+        {
+            string value = string.Empty;
+            if (dic.TryGetValue(key, out value))
+                return value;
+            else
+                return string.Empty;
         }
     }
 }
--- Tests/QTFK.Extensions.Tests/DictionariesExtensionTests.cs
+++ Tests/QTFK.Extensions.Tests/DictionariesExtensionTests.cs
@@ -11,7 +11,7 @@ namespace QTFK.Extensions.Tests
     {
         [TestMethod]
         [TestCategory("Extensions")]
-        public void DictionariesExtension_Tests()
+        public void DictionariesExtension_Tests_1()
         {
             var d = new Dictionary<string, object>
             {
@@ -23,8 +23,11 @@ namespace QTFK.Extensions.Tests
             decimal pepe = 0;
             double doubleNumber = 3;
             double intNumber = 0;
-            d.Get<decimal>("pepe", v => pepe = v);
-            d.Get<string>("tronco", v => tronco = v);
+
+            d
+                .Get<decimal>("pepe", v => pepe = v)
+                .Get<string>("tronco", v => tronco = v)
+                ;
 
             Assert.AreEqual(3.14m, pepe);
             Assert.AreEqual(@"uno que llega", tronco);
@@ -57,13 +60,76 @@ namespace QTFK.Extensions.Tests
 
             string tronco = "", pepe = "";
             string notExists = "pepe";
-            d.Get("pepe", v => pepe = v);
-            d.Get("tronco", v => tronco = v);
+
+            d
+                .Get("pepe", v => pepe = v)
+                .Get("tronco", v => tronco = v)
+                ;
 
             Assert.AreEqual("lorem ipsum dolor shit", pepe);
             Assert.AreEqual(@"uno que llega", tronco);
 
             d.Get("notExist", v => notExists = v);
+            Assert.AreEqual(string.Empty, notExists);
+        }
+
+        [TestMethod]
+        [TestCategory("Extensions")]
+        public void DictionariesExtension_Tests_3()
+        {
+            var d = new Dictionary<string, object>
+            {
+                { "pepe", 3.14m },
+                { "tronco", "uno que llega" },
+            };
+
+            string tronco = "";
+            decimal pepe = 0;
+            double doubleNumber = 3;
+            double intNumber = 0;
+
+            pepe = d.Get<decimal>("pepe");
+            tronco = d.Get<string>("tronco");
+
+            Assert.AreEqual(3.14m, pepe);
+            Assert.AreEqual(@"uno que llega", tronco);
+
+            doubleNumber = d.Get<double>("notExist");
+            Assert.AreEqual(0.0, doubleNumber);
+
+            doubleNumber = d.Get<double>("notExist", 34.0);
+            Assert.AreEqual(34.0, doubleNumber);
+
+            try
+            {
+                intNumber = d.Get<int>("tronco", 55);
+                Assert.Fail("It must fail on wrong data type?");
+            }
+            catch
+            {
+            }
+        }
+
+        [TestMethod]
+        [TestCategory("Extensions")]
+        public void DictionariesExtension_Tests_4()
+        {
+            var d = new Dictionary<string, string>
+            {
+                { "pepe", "lorem ipsum dolor shit" },
+                { "tronco", "uno que llega" },
+            };
+
+            string tronco = "", pepe = "";
+            string notExists = "pepe";
+
+            pepe = d.Get("pepe");
+            tronco = d.Get("tronco");
+
+            Assert.AreEqual("lorem ipsum dolor shit", pepe);
+            Assert.AreEqual(@"uno que llega", tronco);
+
+            notExists = d.Get("notExist");
             Assert.AreEqual(string.Empty, notExists);
         }
     }
