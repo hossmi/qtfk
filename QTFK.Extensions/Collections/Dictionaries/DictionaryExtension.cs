919b0c43f0881e02d21c2028cb7dfab1e190ca4a
Pablo Jimena López <pablo.jimena.lopez@everis.com>
2017-04-19 11:25:20 +0200

    Added DictionaryExtension for easy dictionary Get

---
+++ QTFK.Extensions/Collections/Dictionaries/DictionaryExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QTFK.Extensions.Collections.Dictionaries
+{
+    public static class DictionaryExtension
+    {
+        public static void Get<T>(this IDictionary<string, object> dic, string key, Action<T> setter)
+        {
+            Get<T>(dic, key, default(T), setter);
+        }
+        public static void Get<T>(this IDictionary<string, object> dic, string key, T defaultValue, Action<T> setter)
+        {
+            object value = null;
+            if(dic.TryGetValue(key, out value))
+                setter((T)value);
+            else
+                setter(defaultValue);
+        }
+    }
+}
---
+++ Tests/QTFK.Extensions.Tests/DictionariesExtensionTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using QTFK.Extensions.Collections.Dictionaries;
+
+namespace QTFK.Extensions.Tests
+{
+    [TestClass]
+    public class DictionariesExtensionTests
+    {
+        [TestMethod]
+        [TestCategory("Extensions")]
+        public void DictionariesExtension_Tests()
+        {
+            var d = new Dictionary<string, object>
+            {
+                { "pepe", 3.14m },
+                { "tronco", "uno que llega" },
+            };
+
+            string tronco = "";
+            decimal pepe = 0;
+            double doubleNumber = 3;
+            double intNumber = 0;
+            d.Get<decimal>("pepe", v => pepe = v);
+            d.Get<string>("tronco", v => tronco = v);
+
+            Assert.AreEqual(3.14m, pepe);
+            Assert.AreEqual(@"uno que llega", tronco);
+
+            d.Get<double>("notExist", v => doubleNumber = v);
+            Assert.AreEqual(0.0, doubleNumber);
+
+            d.Get<double>("notExist", 34.0, v => doubleNumber = v);
+            Assert.AreEqual(34.0, doubleNumber);
+
+            try
+            {
+                d.Get<int>("tronco", 55, v => intNumber = v);
+                Assert.Fail("It must fail on wrong data type?");
+            }
+            catch 
+            {
+            }
+        }
+    }
+}
