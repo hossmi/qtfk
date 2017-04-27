0970826d9ab4a0fc0219790b0555655d72d16cf9
hossmi <hossmi@gmail.com>
2017-04-27 20:03:16 +0200

    SQL Server CRUD Queries

---
+++ QTFK.Services.DBIO.SQLServer/Models/DBIO/MsSqlInsertQuery.cs
@@ -0,0 +1,32 @@
+﻿using QTFK.Extensions.Collections.Strings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace QTFK.Models.DBIO
+{
+    public class MsSqlInsertQuery : IDBQuery, IDBQueryWithTableName, IDBQueryWithValuedFields
+    {
+        public MsSqlInsertQuery()
+        {
+            Table = "";
+            ValuedFields = new Dictionary<string, object>();
+        }
+
+        public string Table { get; set; }
+        public IDictionary<string, object> ValuedFields { get; set; }
+
+        public string Compile()
+        {
+            var columnList = ValuedFields
+                .Select(c => new { K = c.Key, V = c.Value.ToString() })
+                .ToList()
+                ;
+
+            return $@"
+                INSERT INTO [{Table}] ({columnList.Stringify(c => $"[{c.K}]")})
+                VALUES ({columnList.Stringify(c => c.V.StartsWith("@") ? c.V : $"'{c.V}'")})
+                ;";
+        }
+    }
+}
---
+++ QTFK.Services.DBIO.SQLServer/Models/DBIO/MsSqlSelectQuery.cs
@@ -0,0 +1,31 @@
+﻿using QTFK.Extensions.Collections.Strings;
+using System;
+using System.Collections.Generic;
+
+namespace QTFK.Models.DBIO
+{
+    public class MsSqlSelectQuery : IDBQuery, IDBQueryWithTableName, IDBQueryWithFields, IDBQueryWithWhere
+    {
+        public MsSqlSelectQuery()
+        {
+            Table = "";
+            Where = "";
+            Fields = new List<string>();
+        }
+
+        public ICollection<string> Fields { get; set; }
+        public string Table { get; set; }
+        public string Where { get; set; }
+
+        public string Compile()
+        {
+            string whereSegment = string.IsNullOrWhiteSpace(Where) ? "" : $"WHERE {Where}";
+
+            return $@"
+                SELECT {Fields.Stringify(f => $"[{f}]")}
+                FROM [{Table}]
+                {whereSegment}
+                ;";
+        }
+    }
+}
