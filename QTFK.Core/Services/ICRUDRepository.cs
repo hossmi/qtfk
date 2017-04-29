b5bdd4630ea39cc6d4c00fcaa09af7b753c018fc
hossmi <hossmi@gmail.com>
2017-04-29 13:20:12 +0200

    Added CRUDRepository interface, abstract class and implementation class

---
+++ QTFK.Core/Services/ICRUDRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QTFK.Services
+{
+    public interface ICRUDRepository<TKey, TItem> where TItem : new()
+    {
+        IQueryable<TItem> Get(Func<TItem, bool> filter);
+        TItem Get(TKey id);
+        TKey Add(TItem item);
+        bool Set(TItem item);
+        bool Remove(TKey id);
+    }
+}
