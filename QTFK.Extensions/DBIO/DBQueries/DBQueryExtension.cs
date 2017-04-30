bc564e597317203aa9091a8684ea519a8a6df9ed
hossmi <hossmi@gmail.com>
2017-04-30 22:35:09 +0200

    DBQuery extension for adding update columns

--- QTFK.Extensions/DBIO/DBQueries/DBQueryExtension.cs
+++ QTFK.Extensions/DBIO/DBQueries/DBQueryExtension.cs
@@ -12,9 +12,37 @@ namespace QTFK.Extensions.DBIO.DBQueries
             return query;
         }
 
-        public static T SetColumn<T>(this T query, string column, object value) where T : IDBQueryWithValuedFields
+        public static T SetColumns<T>(this T query,  params string[] columns) where T : IDBQueryWithValuedFields
         {
-            query.ValuedFields[column] = value;
+            foreach (var column in columns)
+                query.ValuedFields[column] = $"@{column}";
+            return query;
+        }
+
+        /// <summary>
+        /// Adds or sets the column [column] with "'@' + [column value]" as value
+        /// </summary>
+        /// <typeparam name="T">IDBQuery implementation</typeparam>
+        /// <param name="query">IDBQuery object</param>
+        /// <param name="column">name of the column</param>
+        /// <returns>returns the IDBQuery</returns>
+        public static T SetColumn<T>(this T query, string column) where T : IDBQueryWithValuedFields
+        {
+            query.ValuedFields[column] = $"@{column}";
+            return query;
+        }
+
+        /// <summary>
+        /// Adds or sets the column [column] with parameter [parameter] as value
+        /// </summary>
+        /// <typeparam name="T">IDBQuery implementation</typeparam>
+        /// <param name="query">IDBQuery object</param>
+        /// <param name="column">name of the column</param>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public static T SetColumn<T>(this T query, string column, string parameter) where T : IDBQueryWithValuedFields
+        {
+            query.ValuedFields[column] = parameter;
             return query;
         }
 
@@ -34,6 +62,12 @@ namespace QTFK.Extensions.DBIO.DBQueries
         public static T SetWhere<T>(this T query, string where) where T : IDBQueryWithWhere
         {
             query.Where = where;
+            return query;
+        }
+
+        public static T SetTablePrefix<T>(this T query, string prefix) where T : IDBQueryWithTablePrefix
+        {
+            query.Prefix = prefix;
             return query;
         }
 
