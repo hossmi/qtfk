3d849555623e78ced975d34c9632b85eab53c57f
hossmi <hossmi@gmail.com>
2017-05-08 23:45:46 +0200

    Added ICRUDDBIO extending IDBIO. This is the first step towards to IDBIOFactory

---
+++ QTFK.Core/Services/ICRUDDBIO.cs
@@ -0,0 +1,18 @@
+﻿using QTFK.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QTFK.Services
+{
+    public interface ICRUDDBIO : IDBIO
+    {
+        IDBQuerySelect NewSelect();
+        IDBQueryInsert NewInsert();
+        IDBQueryUpdate NewUpdate();
+        IDBQueryDelete NewDelete();
+    }
+}
