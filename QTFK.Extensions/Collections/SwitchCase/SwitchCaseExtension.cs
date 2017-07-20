f8985400c8867e6e7c169fa49444060432cf0558
Hossmi <hossmi@gmail.com>
2017-07-20 18:58:50 +0200

    Added switch case extension and its tests

---
+++ QTFK.Extensions/Collections/SwitchCase/SwitchCaseExtension.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QTFK.Extensions.Collections.SwitchCase
+{
+    public class CaseCollection<T, TResult>
+    {
+        public IEnumerable<T> Items { get; set; }
+        public ICollection<KeyValuePair<Func<T, bool>, Func<T, TResult>>> Cases { get; set; }
+    }
+
+    public static class SwitchCaseExtension
+    {
+        public static CaseCollection<T,TResult> Case<T, TResult>(
+            this IEnumerable<T> items
+            , Func<T, bool> condition
+            , Func<T, TResult> selector
+            )
+        {
+            return new CaseCollection<T, TResult>
+            {
+                Items = items,
+                Cases = new List<KeyValuePair<Func<T, bool>, Func<T, TResult>>>()
+                    .Push(new KeyValuePair<Func<T, bool>, Func<T, TResult>>(condition,selector)),
+            };
+        }
+
+        public static CaseCollection<T, TResult> Case<T, TResult>(
+            this CaseCollection<T, TResult> cases
+            , Func<T, bool> condition
+            , Func<T, TResult> selector
+            )
+        {
+            cases.Cases.Add(new KeyValuePair<Func<T, bool>, Func<T, TResult>>(condition, selector));
+            return cases;
+        }
+
+        public static IEnumerable<TResult> CaseEnd<T, TResult>(
+            this CaseCollection<T, TResult> cases
+            , int matches = 1
+            )
+        {
+            foreach (T item in cases.Items)
+            {
+                int matchesFound = 0;
+                foreach (var option in cases.Cases)
+                {
+                    if (option.Key(item))
+                    {
+                        ++matchesFound;
+                        yield return option.Value(item);
+                    }
+                    if (matchesFound >= matches)
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Tests/QTFK.Extensions.Tests/CollectionsExtensionTest.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QTFK.Extensions.Collections.SwitchCase;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace QTFK.Extensions.Tests
+{
+    [TestClass]
+    public class CollectionsExtensionTest
+    {
+        [TestMethod]
+        [TestCategory("Extensions")]
+        public void SwitchCase_extension_test()
+        {
+            var persons = new[]
+            {
+                new
+                {
+                    Name = "pepe",
+                    LastName = "Flores Silvestre",
+                    Age = 15,
+                    CurrentJob = (string)null
+                },
+                new
+                {
+                    Name = "Rosa",
+                    LastName = "De los vientos Céspedes",
+                    Age = 21,
+                    CurrentJob = "Actress"
+                },
+                new
+                {
+                    Name = "Florencio",
+                    LastName = "Espinosa De las rosas",
+                    Age = 34,
+                    CurrentJob = (string)null
+                },
+                new
+                {
+                    Name = "John",
+                    LastName = "Del Bosque Huertas",
+                    Age = 67,
+                    CurrentJob = "Developer"
+                },
+            };
+
+            var cases = persons
+                .Case(i => i.Age < 18, i => $"{i.Name} is not an adult.") //1
+                .Case(i => i.Age >= 18 && !string.IsNullOrWhiteSpace(i.CurrentJob), i => $"{i.Name} is employed as {i.CurrentJob}.") //2
+                .Case(i => i.Age >= 18 && string.IsNullOrWhiteSpace(i.CurrentJob), i => $"{i.Name} is unemployed.") //1
+                .Case(i => i.Age >= 65 && string.IsNullOrWhiteSpace(i.CurrentJob), i => $"{i.Name} is retired.") //0
+                .Case(i => i.Age >= 65, i => $"{i.Name} is in age of retirement.") //1
+                ;
+
+            var jobs = cases
+                .CaseEnd(int.MaxValue)
+                .ToList()
+                ;
+
+            Assert.AreEqual(5, jobs.Count());
+            Assert.AreEqual(1, jobs.Count(s => s.Contains(" is not an adult.")));
+            Assert.AreEqual(2, jobs.Count(s => s.Contains(" is employed as ")));
+            Assert.AreEqual(1, jobs.Count(s => s.Contains(" is unemployed.")));
+            Assert.AreEqual(0, jobs.Count(s => s.Contains(" is retired.")));
+            Assert.AreEqual(1, jobs.Count(s => s.Contains(" is in age of retirement.")));
+
+            jobs = cases
+                .CaseEnd()
+                .ToList()
+                ;
+
+            Assert.AreEqual(4, jobs.Count());
+            Assert.AreEqual(1, jobs.Count(s => s.Contains(" is not an adult.")));
+            Assert.AreEqual(2, jobs.Count(s => s.Contains(" is employed as ")));
+            Assert.AreEqual(1, jobs.Count(s => s.Contains(" is unemployed.")));
+            Assert.AreEqual(0, jobs.Count(s => s.Contains(" is retired.")));
+            Assert.AreEqual(0, jobs.Count(s => s.Contains(" is in age of retirement.")));
+
+            // from anonymous to anonymous switch-case test
+            var filteredPersons = persons
+                .Case(p => p.Age >= 18 && !string.IsNullOrWhiteSpace(p.CurrentJob), p => new
+                {
+                    FullName = $"{p.LastName}, {p.Name}",
+                    Status = "Employed",
+                })
+                .Case(p => p.CurrentJob != null && p.CurrentJob == "Developer", p => new
+                {
+                    FullName = $"{p.LastName}, {p.Name}",
+                    Status = "Developer",
+                })
+                .CaseEnd(int.MaxValue)
+                .ToList()
+                ;
+
+            Assert.AreEqual(3, filteredPersons.Count());
+            Assert.AreEqual(2, filteredPersons.Count(s => s.Status == "Employed"));
+            Assert.AreEqual(1, filteredPersons.Count(s => s.Status == "Developer"));
+            Assert.AreEqual(2, filteredPersons.Count(s => s.FullName == "Del Bosque Huertas, John"));
+
+        }
+    }
+}
