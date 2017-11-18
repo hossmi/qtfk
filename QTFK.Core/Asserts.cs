0e3c1db6ed35b2d0cf6e559a2860e6318f9e6b10
Hossmi <hossmi@gmail.com>
2017-11-18 14:08:40 +0100

    Added static Asserts class

---
+++ QTFK.Core/Asserts.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+
+namespace QTFK
+{
+    public static class Asserts
+    {
+        public static void IsFilled(string someString, string exceptionMessageFormat, params object[] messageParams)
+        {
+            assertIsFilled(someString, exceptionMessageFormat, messageParams);
+        }
+
+        public static void IsInstance<T>(T item, string exceptionMessageFormat, params object[] messageParams) where T : class
+        {
+            assert(item != null, exceptionMessageFormat, messageParams);
+        }
+
+        public static void IsValidEnum<T>(T enumValue, string exceptionMessageFormat, params object[] messageParams) where T : struct
+        {
+            assert(Enum.IsDefined(typeof(T), enumValue), exceptionMessageFormat, messageParams);
+        }
+
+        public static void IsValidEnum<T>(string enumValue, string exceptionMessageFormat, params object[] messageParams) where T : struct
+        {
+            assertIsFilled(enumValue, exceptionMessageFormat, messageParams);
+            assert(Enum.IsDefined(typeof(T), enumValue), exceptionMessageFormat, messageParams);
+        }
+
+        public static void IsGreaterThanZero(int value, string exceptionMessageFormat, params object[] messageParams)
+        {
+            assert(value > 0, exceptionMessageFormat, messageParams);
+        }
+
+        public static void IsGreaterThanZero(long value, string exceptionMessageFormat, params object[] messageParams)
+        {
+            assert(value > 0, exceptionMessageFormat, messageParams);
+        }
+
+        public static void IsGreaterThanZero(double value, string exceptionMessageFormat, params object[] messageParams)
+        {
+            assert(value > 0.0, exceptionMessageFormat, messageParams);
+        }
+
+        public static void IsGreaterThanZero(decimal value, string exceptionMessageFormat, params object[] messageParams)
+        {
+            assert(value > 0m, exceptionMessageFormat, messageParams);
+        }
+
+        public static void FileExists(string filePath, string exceptionMessageFormat, params object[] messageParams)
+        {
+            assertIsFilled(filePath, exceptionMessageFormat, messageParams);
+            assert(File.Exists(filePath), exceptionMessageFormat, messageParams);
+        }
+
+        public static void IsTrue(bool condition, string exceptionMessageFormat, params object[] messageParams)
+        {
+            assert(condition, exceptionMessageFormat, messageParams);
+        }
+
+        private static void assertIsFilled(string someString, string exceptionMessageFormat, params object[] messageParams)
+        {
+            assert(string.IsNullOrWhiteSpace(someString) == false, exceptionMessageFormat, messageParams);
+        }
+
+        private static void assert(bool condition, string exceptionMessageFormat, params object[] messageParams)
+        {
+            if (condition == false)
+                throw new ArgumentException(string.Format(exceptionMessageFormat ?? "", messageParams));
+        }
+
+    }
+}
---
+++ Tests/QTFK.Core.Tests/AssertTests.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QTFK.Models;
+
+namespace QTFK.Core.Tests
+{
+    [TestClass]
+    public class AssertTests
+    {
+        [TestCategory("Asserts")]
+        [TestMethod]
+        public void Asserting_good_values()
+        {
+            string someString, someLogLevelAsString;
+            int number;
+            Type someType;
+            LogLevel someLogLevel;
+            object someObject;
+
+            someString = "pepe";
+            number = 5;
+            someType = typeof(AssertTests);
+            someObject = someType;
+
+            someLogLevel = LogLevel.Info;
+            someLogLevelAsString = LogLevel.Fatal.ToString();
+
+            Asserts.IsFilled(someString, $"'{nameof(someString)}' cannot be empty.");
+            Asserts.IsGreaterThanZero(number, $"'{nameof(number)}' must be greater than zero.");
+            Asserts.IsInstance(someType, "'{nameof(number)}' cannot be null.");
+            Asserts.IsInstance(someObject, "'{nameof(number)}' cannot be null.");
+            Asserts.IsValidEnum(someLogLevel, "'{nameof(number)}' cannot be null.");
+            Asserts.IsValidEnum<LogLevel>(someLogLevelAsString, "'{nameof(number)}' cannot be null.");
+        }
+
+        [TestCategory("Asserts")]
+        [TestMethod]
+        public void Asserting_bad_values()
+        {
+            string someString, someLogLevelAsString;
+            int number;
+            Type someType;
+            LogLevel someLogLevel;
+            object someObject;
+
+            someString = null;
+            number = 0;
+            someType = null;
+            someObject = someType;
+
+            someLogLevel = (LogLevel)1000;
+            someLogLevelAsString = "Uno_que_llega";
+
+            try
+            {
+                Asserts.IsFilled(someString, $"'{nameof(someString)}' cannot be empty.");
+                Assert.Fail();
+            }
+            catch { }
+
+            try
+            {
+                Asserts.IsGreaterThanZero(number, $"'{nameof(number)}' must be greater than zero.");
+                Assert.Fail();
+            }
+            catch { }
+
+            try
+            {
+                Asserts.IsInstance(someType, "'{nameof(number)}' cannot be null.");
+                Assert.Fail();
+            }
+            catch { }
+
+            try
+            {
+                Asserts.IsInstance(someObject, "'{nameof(number)}' cannot be null.");
+                Assert.Fail();
+            }
+            catch { }
+
+            try
+            {
+                Asserts.IsValidEnum(someLogLevel, "'{nameof(number)}' cannot be null.");
+                Assert.Fail();
+            }
+            catch { }
+
+            try
+            {
+                Asserts.IsValidEnum<LogLevel>(someLogLevelAsString, "'{nameof(number)}' cannot be null.");
+                Assert.Fail();
+            }
+            catch { }
+
+        }
+    }
+}
