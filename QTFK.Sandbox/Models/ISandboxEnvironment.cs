1e305738c92de8660a60cff5f4323bb3826f20c9
Hossmi <hossmi@gmail.com>
2017-11-25 23:16:29 +0100

    Moved all sandbox code to its own QTFK.Sandbox project. The same for test code.

---
+++ QTFK.Sandbox/Models/ISandboxEnvironment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.Remoting;
+
+namespace QTFK.Models
+{
+    public interface ISandboxEnvironment<T> : IDisposable
+        where T : MarshalByRefObject, new()
+    {
+        AppDomain Domain { get; }
+        ObjectHandle Handle { get; }
+        T Instance { get; }
+    }
+}
