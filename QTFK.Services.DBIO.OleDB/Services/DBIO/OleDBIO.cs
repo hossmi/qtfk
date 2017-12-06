9bd22f79b694008d27637376528e416a2f663dd5
Hossmi <hossmi@gmail.com>
2017-12-06 19:55:41 +0100

    Refactoized DBIO implementations using common AbstractDBIO base class

--- QTFK.Services.DBIO.OleDB/Services/DBIO/OleDBIO.cs
+++ QTFK.Services.DBIO.OleDB/Services/DBIO/OleDBIO.cs
@@ -10,183 +10,34 @@ using QTFK.Services.Loggers;
 
 namespace QTFK.Services.DBIO
 {
-    public class OleDBIO : IDBIO, IOleDB
+    public class OleDBIO : AbstractDBIO<OleDbConnection, OleDbCommand, OleDbDataAdapter>, IDBIO, IOleDB
     {
-        private const string ERROR_MESSAGE_DEFAULT = "Error ocurred executing SQL instructions";
-        private const string ERROR_MESSAGE_GETTING_ID = "Error ocurred getting las ID";
-
-        protected readonly string connectionString;
-        protected readonly ILogger<LogLevel> log;
-
         public OleDBIO(string connectionString)
-            : this(connectionString, NullLogger.Instance)
+            : base(connectionString)
         {
         }
 
-        public OleDBIO(
-            string connectionString
-            , ILogger<LogLevel> log
-            )
-        {
-            Asserts.isSomething(log, "Argument 'log' cannot be null.");
-            Asserts.isFilled(connectionString, "Argument 'connectionString' cannot be empty");
-
-            this.connectionString = connectionString;
-            this.log = log;
-        }
-
-        public void Dispose()
+        public OleDBIO(string connectionString, ILogger<LogLevel> log)
+            : base(connectionString, log)
         {
         }
 
-        public DataSet Get(string query, IDictionary<string, object> parameters)
+        protected override OleDbCommand prv_buildCommand(OleDbConnection connection)
         {
-            DataSet ds;
-
-            Asserts.isFilled(query, "Argument 'query' cannot be empty.");
-            Asserts.isSomething(parameters, "Argument 'parameters' cannot be null.");
-
-            ds = null;
-
-            using (OleDbConnection conn = new OleDbConnection(this.connectionString))
-            using (OleDbDataAdapter da = new OleDbDataAdapter(query, conn))
+            return new OleDbCommand()
             {
-                try
-                {
-                    da.SelectCommand.AddParameters(parameters);
-                    ds = new DataSet();
-                    da.Fill(ds);
-                }
-                catch (Exception ex)
-                {
-                    throw prv_wrapException(query, ex, this.log, ERROR_MESSAGE_DEFAULT);
-                }
-                finally
-                {
-                    conn?.Close();
-                }
-            }
-
-            Asserts.isSomething(ds, "Result DataSet is null!");
-            return ds;
+                Connection = connection
+            };
         }
 
-        public IEnumerable<T> Get<T>(string query, IDictionary<string, object> parameters, Func<IDataRecord, T> buildDelegate)
+        protected override OleDbConnection prv_buildConnection(string connectionString)
         {
-            IEnumerable<T> result;
-            OleDbTransaction trans;
-            IDataReader reader;
-
-            Asserts.isFilled(query, "Argument 'query' cannot be empty.");
-            Asserts.isSomething(parameters, "Argument 'parameters' cannot be null.");
-            Asserts.isSomething(buildDelegate, "Argument 'buildDelegate' cannot be null.");
-
-            result = null;
-
-            using (OleDbConnection conn = new OleDbConnection(this.connectionString))
-            using (OleDbCommand command = new OleDbCommand() { Connection = conn })
-            {
-                trans = null;
-                reader = null;
-
-                try
-                {
-                    conn.Open();
-                    trans = conn.BeginTransaction();
-                    command.Transaction = trans;
-                    command.CommandText = query;
-                    command.AddParameters(parameters);
-
-                    reader = command.ExecuteReader();
-                    result = reader
-                        .GetRecords()
-                        .Select(buildDelegate)
-                        .ToList()
-                        ;
-                }
-                catch (Exception ex)
-                {
-                    throw prv_wrapException(command, ex, this.log, ERROR_MESSAGE_DEFAULT);
-                }
-                finally
-                {
-                    if (reader != null && !reader.IsClosed)
-                        reader.Close();
-                    trans?.Rollback();
-                    conn?.Close();
-                }
-            }
-
-            Asserts.isSomething(result, $"Output '{nameof(IEnumerable<T>)}' is null!");
-            return result;
+            return new OleDbConnection(connectionString);
         }
 
-        public object GetLastID(IDbCommand cmd)
+        protected override OleDbDataAdapter prv_buildDataAdapter(string query, OleDbConnection connection)
         {
-            try
-            {
-                return cmd
-                    .SetCommandText(" SELECT @@IDENTITY ")
-                    .ClearParameters()
-                    .ExecuteScalar()
-                    ;
-            }
-            catch (Exception ex)
-            {
-                throw prv_wrapException(cmd, ex, this.log, ERROR_MESSAGE_GETTING_ID);
-            }
-        }
-
-        public int Set(Func<IDbCommand, int> instructions)
-        {
-            int affectedRows;
-            OleDbTransaction trans;
-
-            Asserts.isSomething(instructions, "Argument 'instructions' cannot be null.");
-
-            affectedRows = 0;
-            trans = null;
-
-            using (OleDbConnection conn = new OleDbConnection(this.connectionString))
-            using (OleDbCommand command = new OleDbCommand() { Connection = conn })
-            {
-                try
-                {
-                    conn.Open();
-                    trans = conn.BeginTransaction();
-                    command.Transaction = trans;
-                    affectedRows = instructions(command);
-                    trans.Commit();
-                }
-                catch (Exception ex)
-                {
-                    trans?.Rollback();
-                    throw prv_wrapException(command, ex, this.log, ERROR_MESSAGE_DEFAULT);
-                }
-                finally
-                {
-                    conn?.Close();
-                }
-            }
-
-            return affectedRows;
-        }
-
-        private static DBIOException prv_wrapException(IDbCommand cmd, Exception ex, ILogger<LogLevel> logger, string subject)
-        {
-            return prv_wrapException(cmd?.CommandText ?? "", ex, logger, subject);
-        }
-
-        private static DBIOException prv_wrapException(string query, Exception ex, ILogger<LogLevel> logger, string subject)
-        {
-            string message = $@"{subject}:
-Exception: {ex.GetType().Name}
-Message: {ex.Message}
-Command: '{query}'";
-
-            logger.log(LogLevel.Error, message);
-
-            return new DBIOException(message, ex);
+            return new OleDbDataAdapter(query, connection);
         }
     }
 }
--- QTFK.Services.DBIO.SQLServer/Services/DBIO/SQLServerDBIO.cs
+++ QTFK.Services.DBIO.SQLServer/Services/DBIO/SQLServerDBIO.cs
@@ -13,184 +13,34 @@ using QTFK.Services.Loggers;
 
 namespace QTFK.Services.DBIO
 {
-    public class SQLServerDBIO : IDBIO, ISQLServer
+    public class SQLServerDBIO : AbstractDBIO<SqlConnection, SqlCommand, SqlDataAdapter>, IDBIO, ISQLServer
     {
-        private const string ERROR_MESSAGE_DEFAULT = "Error ocurred executing SQL instructions";
-        private const string ERROR_MESSAGE_GETTING_ID = "Error ocurred getting las ID";
-
-        protected readonly string connectionString;
-        protected readonly ILogger<LogLevel> log;
-
-        public SQLServerDBIO(string connectionString)
-            : this(connectionString, NullLogger.Instance)
+        public SQLServerDBIO(string connectionString) 
+            : base(connectionString)
         {
         }
 
-        public SQLServerDBIO(
-            string connectionString
-            , ILogger<LogLevel> log
-            )
-        {
-            Asserts.isSomething(this.log, "Argument 'log' cannot be null.");
-            Asserts.isFilled(this.connectionString, "Argument 'connectionString' cannot be empty");
-
-            this.connectionString = connectionString;
-            this.log = log;
-        }
-
-        public void Dispose()
+        public SQLServerDBIO(string connectionString, ILogger<LogLevel> log) 
+            : base(connectionString, log)
         {
         }
 
-        public DataSet Get(string query, IDictionary<string, object> parameters)
+        protected override SqlCommand prv_buildCommand(SqlConnection connection)
         {
-            DataSet ds;
-
-            Asserts.isFilled(query, "Argument 'query' cannot be empty.");
-            Asserts.isSomething(parameters, "Argument 'parameters' cannot be null.");
-
-            ds = null;
-
-            using (SqlConnection conn = new SqlConnection(this.connectionString))
-            using (SqlDataAdapter da = new SqlDataAdapter(query, conn))
+            return new SqlCommand()
             {
-                try
-                {
-                    da.SelectCommand.AddParameters(parameters);
-                    ds = new DataSet();
-                    da.Fill(ds);
-                }
-                catch (Exception ex)
-                {
-                    throw prv_wrapException(query, ex, this.log, ERROR_MESSAGE_DEFAULT);
-                }
-                finally
-                {
-                    conn?.Close();
-                }
-            }
-
-            Asserts.isSomething(ds, "Result DataSet is null!");
-            return ds;
+                Connection = connection
+            };
         }
 
-        public IEnumerable<T> Get<T>(string query, IDictionary<string, object> parameters, Func<IDataRecord, T> buildDelegate)
+        protected override SqlConnection prv_buildConnection(string connectionString)
         {
-            IEnumerable<T> result;
-            SqlTransaction trans;
-            IDataReader reader;
-
-            Asserts.isFilled(query, "Argument 'query' cannot be empty.");
-            Asserts.isSomething(parameters, "Argument 'parameters' cannot be null.");
-            Asserts.isSomething(buildDelegate, "Argument 'buildDelegate' cannot be null.");
-
-            result = null;
-
-            using (SqlConnection conn = new SqlConnection(this.connectionString))
-            using (SqlCommand command = new SqlCommand() { Connection = conn })
-            {
-                trans = null;
-                reader = null;
-
-                try
-                {
-                    conn.Open();
-                    trans = conn.BeginTransaction();
-
-                    command.Transaction = trans;
-                    command.CommandText = query;
-                    command.AddParameters(parameters);
-
-                    reader = command.ExecuteReader();
-                    result = reader
-                        .GetRecords()
-                        .Select(buildDelegate)
-                        .ToList()
-                        ;
-                }
-                catch (Exception ex)
-                {
-                    throw prv_wrapException(command, ex, this.log, ERROR_MESSAGE_DEFAULT);
-                }
-                finally
-                {
-                    if (reader != null && !reader.IsClosed)
-                        reader.Close();
-                    trans?.Rollback();
-                    conn?.Close();
-                }
-            }
-
-            Asserts.isSomething(result, $"Output '{nameof(IEnumerable<T>)}' is null!");
-            return result;
+            return new SqlConnection(connectionString);
         }
 
-        public object GetLastID(IDbCommand cmd)
+        protected override SqlDataAdapter prv_buildDataAdapter(string query, SqlConnection connection)
         {
-            try
-            {
-                return cmd
-                    .SetCommandText(" SELECT @@IDENTITY ")
-                    .ClearParameters()
-                    .ExecuteScalar()
-                    ;
-            }
-            catch (Exception ex)
-            {
-                throw prv_wrapException(cmd, ex, this.log, ERROR_MESSAGE_GETTING_ID);
-            }
-        }
-
-        public int Set(Func<IDbCommand, int> instructions)
-        {
-            int affectedRows;
-            SqlTransaction trans ;
-
-            Asserts.isSomething(instructions, "Argument 'instructions' cannot be null.");
-
-            affectedRows = 0;
-
-            using (SqlConnection conn = new SqlConnection(this.connectionString))
-            using (SqlCommand command = new SqlCommand() { Connection = conn })
-            {
-                trans = null;
-
-                try
-                {
-                    conn.Open();
-                    trans = conn.BeginTransaction();
-                    command.Transaction = trans;
-                    affectedRows = instructions(command);
-                    trans.Commit();
-                }
-                catch (Exception ex)
-                {
-                    trans?.Rollback();
-                    throw prv_wrapException(command, ex, this.log, ERROR_MESSAGE_DEFAULT);
-                }
-                finally
-                {
-                    conn?.Close();
-                }
-            }
-            return affectedRows;
-        }
-
-        private static DBIOException prv_wrapException(IDbCommand cmd, Exception ex, ILogger<LogLevel> logger, string subject)
-        {
-            return prv_wrapException(cmd?.CommandText ?? "", ex, logger, subject);
-        }
-
-        private static DBIOException prv_wrapException(string query, Exception ex, ILogger<LogLevel> logger, string subject)
-        {
-            string message = $@"{subject}:
-Exception: {ex.GetType().Name}
-Message: {ex.Message}
-Command: '{query}'";
-
-            logger.log(LogLevel.Error, message);
-
-            return new DBIOException(message, ex);
+            return new SqlDataAdapter(query, connection);
         }
     }
 }
