cb3028bfe36bd32ae18571033bbfa901ce9f7cfc
Hossmi <hossmi@gmail.com>
2017-12-08 12:13:51 +0100

    Added extensions for Type

---
+++ Tests/QTFK.Reflection.Tests/TypeExtensionTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SampleLibrary;
+using QTFK.Extensions.Types;
+
+namespace QTFK.Reflection.Tests
+{
+    [TestClass]
+    [TestCategory("Reflection")]
+    public class TypeExtensionTests
+    {
+        [TestMethod]
+        public void Type_extension_tests()
+        {
+            Type concreteType;
+            Type[] implementedInterfaces;
+
+            concreteType = typeof(ConcreteExtendedSampleService);
+            implementedInterfaces = concreteType.GetInterfaces();
+
+            //Assert.IsTrue(concreteType.implementsInterface(typeof(IExtendedSampleService).FullName));
+            Assert.IsTrue(concreteType.implementsInterface(typeof(IExtendedSampleService)));
+            Assert.IsTrue(concreteType.implementsInterface<IExtendedSampleService>());
+
+            //Assert.IsFalse(concreteType.implementsInterface(typeof(IOtherSampleService).FullName));
+            Assert.IsFalse(concreteType.implementsInterface(typeof(IOtherSampleService)));
+            Assert.IsFalse(concreteType.implementsInterface<IOtherSampleService>());
+
+            //Assert.IsTrue(concreteType.implementsInterface(typeof(ISampleService).FullName));
+            Assert.IsTrue(concreteType.implementsInterface(typeof(ISampleService)));
+            Assert.IsTrue(concreteType.implementsInterface<ISampleService>());
+        }
+    }
+}
