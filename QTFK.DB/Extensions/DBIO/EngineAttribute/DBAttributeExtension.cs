a4c58d6b12e4ec9ca7ad89aa5143afe0bbd8e8d4
Hossmi <hossmi@gmail.com>
2017-12-13 22:37:58 +0100

    Replaced template method by specific methods

--- QTFK.DB/Extensions/DBIO/EngineAttribute/DBAttributeExtension.cs
+++ QTFK.DB/Extensions/DBIO/EngineAttribute/DBAttributeExtension.cs
@@ -1,24 +1,43 @@
 ﻿using QTFK.Attributes;
 using QTFK.Models;
 using QTFK.Services;
+using System;
 using System.Reflection;
 
 namespace QTFK.Extensions.DBIO.EngineAttribute
 {
     public static class DBAttributeExtension
     {
-        public static string getDBEngine<T>(this T item) where T : class, IDBQuery, IQueryFactory, IDBIO
+        public static string getDBEngine(this IDBIO db)
+        {
+            return prv_getDBEngine(db);
+        }
+
+        public static string getDBEngine(this IQueryFactory queryFactory)
+        {
+            return prv_getDBEngine(queryFactory);
+        }
+
+        public static string getDBEngine(this IDBQuery query)
+        {
+            return prv_getDBEngine(query);
+        }
+
+        private static string prv_getDBEngine(object item)
         {
             DBAttribute dbAttribute;
+            string engine;
+            Type itemType;
 
             Asserts.isSomething(item, $"Parameter '{nameof(item)}' cannot be null.");
 
-            dbAttribute = item
-                .GetType()
-                .GetCustomAttribute<DBAttribute>();
+            itemType = item.GetType();
+            dbAttribute = itemType.GetCustomAttribute<DBAttribute>();
 
-            Asserts.isSomething(dbAttribute, $"Class '{typeof(T).FullName}' has no DBAttribute.");
-            return dbAttribute.Engine;
+            Asserts.isSomething(dbAttribute, $"Class '{itemType.FullName}' has no {nameof(DBAttribute)}.");
+            engine = dbAttribute.Engine;
+
+            return engine;
         }
     }
 }
