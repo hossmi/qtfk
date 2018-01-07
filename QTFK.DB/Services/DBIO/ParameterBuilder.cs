0027571c4023066d942917b9233cd34d2bf4a336
Hossmi <hossmi@gmail.com>
2018-01-07 20:50:16 +0100

    Updated ParameterBuilder abstract class

--- QTFK.DB/Services/DBIO/ParameterBuilder.cs
+++ QTFK.DB/Services/DBIO/ParameterBuilder.cs
@@ -1,27 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace QTFK.Services.DBIO
 {
     public abstract class ParameterBuilder : IParameterBuilder
     {
+        protected IDictionary<string, int> parameters;
+
         private static IParameterBuilder tsqlParameterBuilder;
+
+        public ParameterBuilder()
+        {
+            this.parameters = new Dictionary<string, int>();
+        }
 
         public virtual string buildParameter(string parameterName)
         {
             string result;
             string parameter;
             char[] validChars;
+            int number;
 
             validChars = parameterName
                 .Where(c => c > 32)
                 .ToArray();
 
             parameter = new string(validChars);
-            result = $"@{parameter}";
+
+            number = (this.parameters.ContainsKey(parameter) ? this.parameters[parameter] : 0) + 1;
+            result = $"@{parameter}{number}";
+            this.parameters[parameter] = number;
 
             return result;
         }
