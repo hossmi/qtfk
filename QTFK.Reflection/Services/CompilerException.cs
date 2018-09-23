957584c20ab8fa70af86bfef21185414fa1c8b8e
Hossmi <hossmi@gmail.com>
2018-09-23 08:49:16 +0200

    Added CompilerException

---
+++ QTFK.Reflection/Services/CompilerException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Runtime.Serialization;
+
+namespace QTFK.Services
+{
+    [Serializable]
+    public class CompilerException : Exception
+    {
+        protected CompilerException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+        public CompilerException(CompilerErrorCollection errors) : base("Error compiling code. See Errors property for details.")
+        {
+            this.Errors = errors;
+        }
+
+        public CompilerErrorCollection Errors { get; }
+    }
+}
