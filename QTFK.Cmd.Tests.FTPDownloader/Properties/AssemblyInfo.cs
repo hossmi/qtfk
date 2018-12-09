2da63c61d64b56a869d755aef87c0cb7aa38f0d0
hossmi <hossmi@gmail.com>
2018-12-09 08:34:37 +0100

    Rename FTPDownloader project name to QTFK.Cmd.Tests.FTPDownloader

--- QTFK.Cmd.Tests.FTPDownloader/Properties/AssemblyInfo.cs
+++ QTFK.Cmd.Tests.FTPDownloader/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FTPDownloader")]
+[assembly: AssemblyTitle("QTFK.Cmd.Tests.FTPDownloader")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Hossmi")]
