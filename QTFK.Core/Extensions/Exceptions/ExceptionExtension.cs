7bd29e8de67ff1f973cbd372a8b7f76123b7b9f1
hossmi <hossmi@gmail.com>
2018-12-09 20:22:08 +0100

    Refactor ExceptionExtension wrap method

--- QTFK.Core/Extensions/Exceptions/ExceptionExtension.cs
+++ QTFK.Core/Extensions/Exceptions/ExceptionExtension.cs
@@ -1,16 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace QTFK.Extensions.Exceptions
 {
     public static class ExceptionExtension
     {
-        public static T wrap<T>(this Exception exception, Func<Exception, T> wrapperDelegate) where T : Exception
+        //public static T wrap<T>(this Exception exception, Func<Exception, T> wrapperDelegate) where T : Exception
+        //{
+        //    T wrappedException;
+
+        //    Asserts.isNotNull(wrapperDelegate);
+
+        //    wrappedException = wrapperDelegate(exception);
+        //    Asserts.isNotNull(wrappedException);
+
+        //    return wrappedException;
+        //}
+
+        public static TExOut wrap<TExOut, TExIn>(this TExIn exception, Func<TExIn, TExOut> wrapperDelegate) 
+            where TExIn : Exception
+            where TExOut : Exception
         {
-            T wrappedException;
+            TExOut wrappedException;
 
             Asserts.isNotNull(wrapperDelegate);
 
