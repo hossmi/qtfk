b53d9e751f1a7daf13b79f759621b014b54f77b3
hossmi <hossmi@gmail.com>
2019-01-13 09:10:31 +0100

    Add random extensions

--- QTFK.Core/QTFK/Extensions/Randoms/RandomExtensions.cs
+++ QTFK.Core/QTFK/Extensions/Randoms/RandomExtensions.cs
@@ -36,6 +36,45 @@ namespace QTFK.Extensions.Randoms
             return items;
         }
 
+        public static IEnumerable<T> shuffle<T>(this IEnumerable<T> items, Random random)
+        {
+            return prv_shuffle<T>(items, random, random.Next());
+        }
+
+        public static IEnumerable<T> shuffle<T>(this IEnumerable<T> items, Random random, int bufferSize)
+        {
+            return prv_shuffle<T>(items, random, bufferSize);
+        }
+
+        private static IEnumerable<T> prv_shuffle<T>(this IEnumerable<T> items, Random random, int bufferSize)
+        {
+            T[] buffer = new T[bufferSize];
+            IEnumerator<T> enumerator = items.GetEnumerator();
+            int itemCount;
+
+            for (itemCount = 0; itemCount < bufferSize; itemCount++)
+            {
+                if (enumerator.MoveNext())
+                    buffer[itemCount] = enumerator.Current;
+                else
+                    break;
+            }
+
+            if (itemCount <= 0)
+                yield break;
+
+            while(enumerator.MoveNext())
+            {
+                int nextItem = random.Next(itemCount);
+                yield return buffer[nextItem];
+                buffer[nextItem] = enumerator.Current;
+            }
+
+            for (int i = 0; i < itemCount; i++)
+                yield return buffer[i];
+
+        }
+
         private static void prv_shuffle<T>(Random random, T[] items)
         {
             int n = random.Next(0, items.Length);
